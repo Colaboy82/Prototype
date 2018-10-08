f6d071854656df8902a837e3c5ad3cce748f495a
derrickmu <colaboy82@gmail.com>
2018-10-08 14:20:20 -0400

    Initial Commit

---
+++ Consent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Consent
+//
+//  Created by Grumpy1211 on 10/8/18.
+//  Copyright © 2018 MuSquared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
