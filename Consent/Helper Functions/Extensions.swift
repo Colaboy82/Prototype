944523c256a40b9dc11dd132decfd2f2058c5a81
Master <grumpy1211@grumpy1211s-air.home>
2018-12-29 00:53:38 -0500

    added an extensions file, deleted extraneous uidesignablex folder

---
+++ Consent/Helper Functions/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Button.swift
+//  Consent
+//
+//  Created by Grumpy1211 on 12/24/18.
+//  Copyright © 2018 MuSquared. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+
