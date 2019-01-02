7a302c2f3aa6d5b02cc7437e7d446bf74731b517
Master <grumpy1211@grumpy1211s-air.home>
2019-01-01 22:02:03 -0500

    structured new Files to prepare to program other components of the app

---
+++ Consent/Main/ProfilePage/ProfilePageVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilePageVC.swift
+//  Consent
+//
+//  Created by Grumpy1211 on 1/1/19.
+//  Copyright © 2019 MuSquared. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ProfilePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Consent/Main/TableView.swift
@@ -0,0 +1,22 @@
+//
+//  TableView.swift
+//  Consent
+//
+//  Created by Grumpy1211 on 1/1/19.
+//  Copyright © 2019 MuSquared. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class TableView: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
