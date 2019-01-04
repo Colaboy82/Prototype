213e96111d8c69c9e2575c20f2e9fa1241da5e28
Master <grumpy1211@grumpy1211s-air.home>
2019-01-04 15:42:41 -0500

    added files for the confirmation consent pop up

---
+++ Consent/PopUps/ConfirmConsentPopUp.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmConsentPopUp.swift
+//  Consent
+//
+//  Created by Grumpy1211 on 1/4/19.
+//  Copyright © 2019 MuSquared. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmConsentPopUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
